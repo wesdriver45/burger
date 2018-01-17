92e08ec42dc0e7bb5a254570aafd9a1bfee8f411
wesdriver45 <wesdriver45@gmail.com>
2018-01-16 20:42:35 -0500

    changes

--- server.js
+++ server.js
@@ -3,3 +3,23 @@ var methodOveride = require("method-overide");
 var bodyParser = require("body-parser");
 
 var app = express();
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PROT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ etended: false }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
+
+
