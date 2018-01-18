1b8a0ab02f1003392c0922bac5d59c897119c144
wesdriver45 <wesdriver45@gmail.com>
2018-01-18 09:16:58 -0500

    sql changes

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers (
+CREATE TABLE burgers
+(
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(100) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
