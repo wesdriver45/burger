c40848b1656581abc349a97fc00bb7c783a3f44f
wesdriver45 <wesdriver45@gmail.com>
2018-01-20 08:13:59 -0500

    routes/index

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var password = require("../password.js")
 
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
-  password: "All4myFam",
+  password: password,
   database: "burgers_db"
 });
 
