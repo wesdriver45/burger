0ccfa6bb0618e933a8d5da5390771ec2829b7e6b
wesdriver45 <wesdriver45@gmail.com>
2018-01-20 08:53:14 -0500

    cbs/index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,12 +7,12 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(burger) {
-    var hbsObject = {
+  burger.selectAll(function(data) {
+    var burger = {
       burgers: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    console.log(burger);
+    res.render("index", burger);
   });
 });
 
@@ -44,18 +44,18 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/cats/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   cat.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
